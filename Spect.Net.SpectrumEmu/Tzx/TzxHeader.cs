7c96dce8b8fc2e44bdd258ee49a97b19b3b032e2
Istvan <Istvan@INOVAKSPRO3>
2017-03-07 13:30:06 +0100

    Sound development started

---
+++ Spect.Net.SpectrumEmu/Tzx/TzxHeader.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Spect.Net.SpectrumEmu.Tzx
+{
+    [StructLayout(LayoutKind.Explicit)]
+    public class TzxHeader
+    {
+        [FieldOffset(0)] private byte[] Signature;
+    }
+}
