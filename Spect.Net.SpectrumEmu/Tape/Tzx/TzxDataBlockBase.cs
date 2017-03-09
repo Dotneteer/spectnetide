5668e18887c844bed274a47dd9e55860fbf57a0d
Istvan Novak <dotneteer@hotmail.com>
2017-03-09 11:31:22 +0100

    TZX file handling implementation started.

---
+++ Spect.Net.SpectrumEmu/Tape/Tzx/TzxDataBlockBase.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace Spect.Net.SpectrumEmu.Tape.Tzx
+{
+    /// <summary>
+    /// This class describes a TZX Block
+    /// </summary>
+    public abstract class TzxDataBlockBase
+    {
+        /// <summary>
+        /// The ID of the block
+        /// </summary>
+        public abstract int BlockId { get; }
+
+        /// <summary>
+        /// Reads the content of the block from the specified binary stream.
+        /// </summary>
+        /// <param name="reader">Stream to read the block from</param>
+        public abstract void ReadFrom(BinaryReader reader);
+
+        /// <summary>
+        /// Writes the content of the block to the specified binary stream.
+        /// </summary>
+        /// <param name="writer">Stream to write the block to</param>
+        public abstract void WriteTo(BinaryWriter writer);
+
+        /// <summary>
+        /// Override this method to check the content of the block
+        /// </summary>
+        public virtual bool IsValid => true;
+    }
+}
---
+++ Spect.Net.SpectrumEmu/Tape/Tzx/TzxStandardSpeedDataBlock.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+
+namespace Spect.Net.SpectrumEmu.Tape.Tzx
+{
+    /// <summary>
+    /// Represents the standard speed data block in a TZX file
+    /// </summary>
+    public class TzxStandardSpeedDataBlock : TzxDataBlockBase
+    {
+        /// <summary>
+        /// Pause after this block
+        /// </summary>
+        public ushort PauseAfter { get; set; }
+
+        /// <summary>
+        /// Lenght of block data
+        /// </summary>
+        public ushort DataLenght { get; set; }
+
+        /// <summary>
+        /// Block Data
+        /// </summary>
+        public byte[] Data { get; set; }
+
+        #region Overrides of TzxDataBlockBase
+
+        /// <summary>
+        /// The ID of the block
+        /// </summary>
+        public override int BlockId => 0x10;
+
+        /// <summary>
+        /// Reads the content of the block from the specified binary stream.
+        /// </summary>
+        /// <param name="reader">Stream to read the block from</param>
+        public override void ReadFrom(BinaryReader reader)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes the content of the block to the specified binary stream.
+        /// </summary>
+        /// <param name="writer">Stream to write the block to</param>
+        public override void WriteTo(BinaryWriter writer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+    }
+}
