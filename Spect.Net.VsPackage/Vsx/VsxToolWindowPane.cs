8622f009b4a60419855c8bb25b92f30f8231d5fe
Istvan Novak <dotneteer@hotmail.com>
2017-08-09 11:25:19 +0200

    Keyboard handling in the Spectrum Emulator tool window is being refactored.

--- Spect.Net.VsPackage/Vsx/VsxToolWindowPane.cs
+++ Spect.Net.VsPackage/Vsx/VsxToolWindowPane.cs
@@ -2,7 +2,10 @@
 using System.ComponentModel.Design;
 using System.Reflection;
 using System.Windows.Controls;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
 // ReSharper disable VirtualMemberCallInConstructor
 
 namespace Spect.Net.VsPackage.Vsx
@@ -10,7 +13,7 @@ namespace Spect.Net.VsPackage.Vsx
     /// <summary>
     /// This class represents the base class of a VSX window pane
     /// </summary>
-    public abstract class VsxToolWindowPane<TPackage, TControl>: ToolWindowPane
+    public abstract class VsxToolWindowPane<TPackage, TControl>: ToolWindowPane, IVsWindowFrameNotify3
         where TPackage: VsxPackage
         where TControl: ContentControl, new()
     {
@@ -54,6 +57,56 @@ namespace Spect.Net.VsPackage.Vsx
                     ToolBarLocation = (int)toolBarAttr.Location;
                 }
             }
+        }
+
+        /// <summary>
+        /// Prepares window frame events
+        /// </summary>
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            InvokeFrameMethod(f => f.SetProperty((int)__VSFPROPID.VSFPROPID_ViewHelper, this));
+        }
+
+        /// <summary>Notifies the VSPackage of a change in the window's display state.</summary>
+        /// <param name="fShow">[in] Specifies the reason for the display state change. Value taken from the <see cref="T:Microsoft.VisualStudio.Shell.Interop.__FRAMESHOW" /> enumeration.</param>
+        /// <returns>If the method succeeds, it returns <see cref="F:Microsoft.VisualStudio.VSConstants.S_OK" />. If it fails, it returns an error code.</returns>
+        public virtual int OnShow(int fShow) => VSConstants.S_OK;
+
+        /// <summary>Notifies the VSPackage that a window is being moved.</summary>
+        /// <param name="x">[in] New horizontal position.</param>
+        /// <param name="y">[in] New vertical position.</param>
+        /// <param name="w">[in] New window width.</param>
+        /// <param name="h">[in] New window height.</param>
+        /// <returns>If the method succeeds, it returns <see cref="F:Microsoft.VisualStudio.VSConstants.S_OK" />. If it fails, it returns an error code.</returns>
+        public virtual int OnMove(int x, int y, int w, int h) => VSConstants.S_OK;
+
+        /// <summary>Notifies the VSPackage that a window is being resized.</summary>
+        /// <param name="x">[in] New horizontal position.</param>
+        /// <param name="y">[in] New vertical position.</param>
+        /// <param name="w">[in] New window width.</param>
+        /// <param name="h">[in] New window height.</param>
+        /// <returns>If the method succeeds, it returns <see cref="F:Microsoft.VisualStudio.VSConstants.S_OK" />. If it fails, it returns an error code.</returns>
+        public int OnSize(int x, int y, int w, int h) => VSConstants.S_OK;
+
+        /// <summary>Notifies the VSPackage that a window's docked state is being altered.</summary>
+        /// <param name="fDockable">[in] <see langword="true" /> if the window frame is being docked.</param>
+        /// <param name="x">[in] Horizontal position of undocked window.</param>
+        /// <param name="y">[in] Vertical position of undocked window.</param>
+        /// <param name="w">[in] Width of undocked window.</param>
+        /// <param name="h">[in] Height of undocked window.</param>
+        /// <returns>If the method succeeds, it returns <see cref="F:Microsoft.VisualStudio.VSConstants.S_OK" />. If it fails, it returns an error code.</returns>
+        public int OnDockableChange(int fDockable, int x, int y, int w, int h) => VSConstants.S_OK;
+
+        /// <summary>Notifies the VSPackage that a window frame is closing and tells the environment what action to take.</summary>
+        /// <param name="pgrfSaveOptions">[in, out] Specifies options for saving window content. Values are taken from the <see cref="T:Microsoft.VisualStudio.Shell.Interop.__FRAMECLOSE" /> enumeration.</param>
+        /// <returns>If the method succeeds, it returns <see cref="F:Microsoft.VisualStudio.VSConstants.S_OK" />. If it fails, it returns an error code.</returns>
+        public int OnClose(ref uint pgrfSaveOptions) => VSConstants.S_OK;
+
+        private void InvokeFrameMethod(Action<IVsWindowFrame> action)
+        {
+            var windowFrame = Frame as IVsWindowFrame;
+            if (windowFrame != null) action(windowFrame);
         }
     }
 }
--- Spect.Net.Wpf/SpectrumControl/SpectrumDisplayControl.xaml.cs
+++ Spect.Net.Wpf/SpectrumControl/SpectrumDisplayControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
