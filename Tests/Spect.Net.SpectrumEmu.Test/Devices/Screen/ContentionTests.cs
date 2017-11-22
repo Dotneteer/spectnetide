80bce990b723245cb143fd76b93b59fc7bd363f6
Istvan Novak <dotneteer@hotmail.com>
2017-11-22 07:21:23 +0100

    Additional I/O contention tests added.

--- Tests/Spect.Net.SpectrumEmu.Test/Devices/Screen/ContentionTests.cs
+++ Tests/Spect.Net.SpectrumEmu.Test/Devices/Screen/ContentionTests.cs
@@ -584,6 +584,153 @@ namespace Spect.Net.SpectrumEmu.Test.Devices.Screen
             spectrum.Cpu.Tacts.ShouldBe(expected + tactToReach + 4); // +4 for HALT
         }
 
+        [TestMethod]
+        public void NoIncCContentionValueOnPort0XfeIsAppliedWhileFirstDisplayLineIsReached()
+        {
+            // --- Arrange
+            const int EXPECTED_CONTENTION = 0;
+            var screenConfig = new ScreenConfiguration(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var pixels = new TestPixelRenderer(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var spectrum = new SpectrumAdvancedTestMachine(pixels);
+
+            var tactToReach = screenConfig.FirstDisplayLine * screenConfig.ScreenLineTime;
+            InitMachineInCWithNops(tactToReach, 0xFE, spectrum);
+
+            // --- Act
+            spectrum.ExecuteCycle(CancellationToken.None, new ExecuteCycleOptions(EmulationMode.UntilHalt));
+
+            // --- Assert
+            spectrum.Cpu.Tacts.ShouldBe(EXPECTED_CONTENTION + tactToReach + 4); // +4 for HALT
+        }
+
+        [TestMethod]
+        public void NoInCContentionValueOnPort0XfeIsAppliedWhileLeftDisplayEdgeIsReached()
+        {
+            // --- Arrange
+            const int EXPECTED_CONTENTION = 0;
+            var screenConfig = new ScreenConfiguration(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var pixels = new TestPixelRenderer(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var spectrum = new SpectrumAdvancedTestMachine(pixels);
+
+            var tactToReach = screenConfig.FirstDisplayLine * screenConfig.ScreenLineTime
+                              + screenConfig.FirstPixelTactInLine - 4;
+            InitMachineInCWithNops(tactToReach, 0xFE, spectrum);
+
+            // --- Act
+            spectrum.ExecuteCycle(CancellationToken.None, new ExecuteCycleOptions(EmulationMode.UntilHalt));
+
+            // --- Assert
+            spectrum.Cpu.Tacts.ShouldBe(EXPECTED_CONTENTION + tactToReach + 4); // +4 for HALT
+        }
+
+        [TestMethod]
+        [DataRow(1, 3)]
+        [DataRow(2, 2)]
+        [DataRow(3, 1)]
+        [DataRow(4, 0)]
+        [DataRow(5, 0)]
+        [DataRow(6, 6)]
+        [DataRow(7, 5)]
+        [DataRow(8, 4)]
+        public void InCContentionValueOnPort0XfeIsAppliedWhenLeftDisplayEdgeIsReached(int deviation, int expected)
+        {
+            // --- Arrange
+            var screenConfig = new ScreenConfiguration(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var pixels = new TestPixelRenderer(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var spectrum = new SpectrumAdvancedTestMachine(pixels);
+
+            var tactToReach = screenConfig.FirstDisplayLine * screenConfig.ScreenLineTime
+                              + screenConfig.FirstPixelTactInLine + deviation;
+            InitMachineInCWithNops(tactToReach, 0xfe, spectrum);
+
+            // --- Act
+            spectrum.ExecuteCycle(CancellationToken.None, new ExecuteCycleOptions(EmulationMode.UntilHalt));
+
+            // --- Assert
+            spectrum.Cpu.Tacts.ShouldBe(expected + tactToReach + 4); // +4 for HALT
+        }
+
+        [TestMethod]
+        [DataRow(1, 4)]
+        [DataRow(2, 3)]
+        [DataRow(3, 2)]
+        [DataRow(4, 1)]
+        [DataRow(5, 0)]
+        [DataRow(6, 0)]
+        [DataRow(7, 6)]
+        [DataRow(8, 5)]
+        public void InCContentionValueOnPort0X40IsAppliedWhenLeftDisplayEdgeIsReached(int deviation, int expected)
+        {
+            // --- Arrange
+            var screenConfig = new ScreenConfiguration(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var pixels = new TestPixelRenderer(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var spectrum = new SpectrumAdvancedTestMachine(pixels);
+
+            var tactToReach = screenConfig.FirstDisplayLine * screenConfig.ScreenLineTime
+                              + screenConfig.FirstPixelTactInLine + deviation;
+            InitMachineInCWithNops(tactToReach, 0x40, spectrum);
+
+            // --- Act
+            spectrum.ExecuteCycle(CancellationToken.None, new ExecuteCycleOptions(EmulationMode.UntilHalt));
+
+            // --- Assert
+            spectrum.Cpu.Tacts.ShouldBe(expected + tactToReach + 4); // +4 for HALT
+        }
+
+        [TestMethod]
+        [DataRow(1, 4)]
+        [DataRow(2, 3)]
+        [DataRow(3, 2)]
+        [DataRow(4, 1)]
+        [DataRow(5, 0)]
+        [DataRow(6, 0)]
+        [DataRow(7, 6)]
+        [DataRow(8, 5)]
+        public void InCContentionValueOnPort0X41IsAppliedWhenLeftDisplayEdgeIsReached(int deviation, int expected)
+        {
+            // --- Arrange
+            var screenConfig = new ScreenConfiguration(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var pixels = new TestPixelRenderer(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var spectrum = new SpectrumAdvancedTestMachine(pixels);
+
+            var tactToReach = screenConfig.FirstDisplayLine * screenConfig.ScreenLineTime
+                              + screenConfig.FirstPixelTactInLine + deviation;
+            InitMachineInCWithNops(tactToReach, 0x41, spectrum);
+
+            // --- Act
+            spectrum.ExecuteCycle(CancellationToken.None, new ExecuteCycleOptions(EmulationMode.UntilHalt));
+
+            // --- Assert
+            spectrum.Cpu.Tacts.ShouldBe(expected + tactToReach + 4); // +4 for HALT
+        }
+
+        [TestMethod]
+        [DataRow(1, 0)]
+        [DataRow(2, 0)]
+        [DataRow(3, 0)]
+        [DataRow(4, 0)]
+        [DataRow(5, 0)]
+        [DataRow(6, 0)]
+        [DataRow(7, 0)]
+        [DataRow(8, 0)]
+        public void NoInCContentionValueOnPort0XffIsAppliedWhenLeftDisplayEdgeIsReached(int deviation, int expected)
+        {
+            // --- Arrange
+            var screenConfig = new ScreenConfiguration(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var pixels = new TestPixelRenderer(SpectrumModels.ZxSpectrum48Pal.Screen);
+            var spectrum = new SpectrumAdvancedTestMachine(pixels);
+
+            var tactToReach = screenConfig.FirstDisplayLine * screenConfig.ScreenLineTime
+                              + screenConfig.FirstPixelTactInLine - 12 + deviation;
+            InitMachineInCWithNops(tactToReach, 0xff, spectrum);
+
+            // --- Act
+            spectrum.ExecuteCycle(CancellationToken.None, new ExecuteCycleOptions(EmulationMode.UntilHalt));
+
+            // --- Assert
+            spectrum.Cpu.Tacts.ShouldBe(expected + tactToReach + 4); // +4 for HALT
+        }
+
 
 
 
@@ -815,5 +962,53 @@ namespace Spect.Net.SpectrumEmu.Test.Devices.Screen
 
             spectrum.InitCode(code);
         }
+
+        private static void InitMachineInCWithNops(int tactToReach, byte port, SpectrumAdvancedTestMachine spectrum)
+        {
+            var code = new List<byte>
+            {
+                0xF3 // DI ; (4) 
+            };
+            var usedTacts = 4;
+            var remainder = tactToReach % 4;
+            switch (remainder)
+            {
+                case 0:
+                    code.Add(0x03); // INC BC (6)
+                    usedTacts += 6;
+                    break;
+                case 1:
+                    code.Add(0x3E); // LD A,0 (7)
+                    code.Add(0x00);
+                    usedTacts += 7;
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    code.Add(0xED); // LD A,I (9)
+                    code.Add(0x57);
+                    usedTacts += 9;
+                    break;
+            }
+
+            usedTacts += 26;
+            var extraNops = (tactToReach - usedTacts) / 4;
+            for (var i = 0; i < extraNops; i++)
+            {
+                code.Add(0x00); // NOP
+            }
+
+            // --- Now we add the instruction for contention
+            code.Add(0x06); // LD B,port ; (7)
+            code.Add(port);
+            code.Add(0x0E); // LD C,port ; (7)
+            code.Add(port);
+            code.Add(0xED); // IN A,(C) ; (12)
+            code.Add(0x78);
+            code.Add(0x76); // HALT
+
+            spectrum.InitCode(code);
+        }
+
     }
 }
