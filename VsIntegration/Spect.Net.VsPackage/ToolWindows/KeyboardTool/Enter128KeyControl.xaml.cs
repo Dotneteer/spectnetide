48541b9544200149d58a50b6edaec4c9c5e26b74
Istvan Novak <dotneteer@hotmail.com>
2017-12-14 10:22:53 +0100

    Spectrum 128 keyboard layout complete

---
+++ VsIntegration/Spect.Net.VsPackage/ToolWindows/KeyboardTool/Enter128KeyControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace Spect.Net.VsPackage.ToolWindows.KeyboardTool
+{
+    /// <summary>
+    /// Interaction logic for Single128KeyControl.xaml
+    /// </summary>
+    public partial class Enter128KeyControl
+    {
+        /// <summary>
+        /// Responds to the event when the main key is clicked
+        /// </summary>
+        public event MouseButtonEventHandler MainKeyClicked;
+
+        public Enter128KeyControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
