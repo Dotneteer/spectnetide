b4e75ef928dc0d565c0fc18e5fa5f49c62340e27
Jason Salter <jasonsalter@outlook.com>
2018-03-30 23:37:17 +0100

    Removed hardcoded test path

--- Tests/Spect.Net.SpectrumEmu.Test/Utility/RomSplitterTest.cs
+++ Tests/Spect.Net.SpectrumEmu.Test/Utility/RomSplitterTest.cs
@@ -8,13 +8,11 @@ namespace Spect.Net.SpectrumEmu.Test.Utility
     [TestClass]
     public class RomSplitterTest
     {
-        public const string SOURCEFILE =
-            @"C:\Users\dotne\source\repos\spectnetide\TBBlue\enNextOS.rom";
-
         [TestMethod]
+        [DeploymentItem("enNextOS.rom")]
         public void SplitRomFile()
         {
-            var fullRom = File.ReadAllBytes(SOURCEFILE);
+            var fullRom = File.ReadAllBytes("enNextOS.rom");
             for (var i = 0; i < 4; i++)
             {
                 var rom = fullRom.Skip(i * 0x4000).Take(0x4000).ToArray();
@@ -24,13 +22,14 @@ namespace Spect.Net.SpectrumEmu.Test.Utility
         }
 
         [TestMethod]
+        [DeploymentItem("enNextOS.rom")]
         public void SearchForBadMmc()
         {
             //var pattern = new byte[] {0x6D, 0x6D, 0x63, 0x2E, 0x72};
             var pattern = new byte[] { 0xC8, 0x3D };
             var matchIdx = 0;
             var startAddr = 0;
-            var fullRom = File.ReadAllBytes(SOURCEFILE);
+            var fullRom = File.ReadAllBytes("enNextOS.rom");
             for (var i = 0; i < fullRom.Length - 10; i++)
             {
                 if (pattern[matchIdx] != fullRom[i])
