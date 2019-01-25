11d107b186546b272224e7bff21fcd1e1e1b2ca0
Istvan Novak <dotneteer@hotmail.com>
2019-01-25 07:56:05 +0100

    Watch window is ready for item evaluation

--- VsIntegration/Spect.Net.VsPackage/ToolWindows/Watch/WatchItemViewModel.cs
+++ VsIntegration/Spect.Net.VsPackage/ToolWindows/Watch/WatchItemViewModel.cs
@@ -13,11 +13,21 @@ namespace Spect.Net.VsPackage.ToolWindows.Watch
         private bool _hasError;
         private WatchToolWindowViewModel _parent;
         private int _seqNo;
+        private string _format;
 
         public int SeqNo
         {
             get => _seqNo;
             set => Set(ref _seqNo, value);
+        }
+
+        /// <summary>
+        /// The format of the expression
+        /// </summary>
+        public string Format
+        {
+            get => _format;
+            set => Set(ref _format, value);
         }
 
         /// <summary>
@@ -56,19 +66,75 @@ namespace Spect.Net.VsPackage.ToolWindows.Watch
             set => Set(ref _parent, value);
         }
 
+        /// <summary>
+        /// Tests if this item can move up
+        /// </summary>
+        public bool CanMoveUp => SeqNo < Parent.WatchItems.Count;
+
+        /// <summary>
+        /// Tests if this item can move down
+        /// </summary>
+        public bool CanMoveDown => SeqNo > 1;
+
+        /// <summary>
+        /// Notifies the parent to remove this item
+        /// </summary>
+        public RelayCommand RemoveCommand { get; set; }
+
+        /// <summary>
+        /// Notifies the parent that this item should be moved up
+        /// </summary>
+        public RelayCommand MoveUpCommand { get; set; }
+
+        /// <summary>
+        /// Notifies the parent that this item should be moved down
+        /// </summary>
+        public RelayCommand MoveDownCommand { get; set; }
+
+        /// <summary>
+        /// Initializes the view model
+        /// </summary>
         public WatchItemViewModel()
         {
-            //if (IsInDesignMode)
-            //{
+            if (IsInDesignMode)
+            {
                 Expression = "(34AC)";
+                Format = "byte";
                 Value = "003457683";
                 HasError = false;
-            //}
+            }
+            RemoveCommand = new RelayCommand(() =>
+            {
+                Parent.RemoveWatchItem(this);
+            });
+            MoveUpCommand = new RelayCommand(() =>
+            {
+                Parent.MoveUpWatchItem(this);
+            });
+            MoveDownCommand = new RelayCommand(() =>
+            {
+                Parent.MoveDownWatchItem(this);
+            });
         }
 
+        /// <summary>
+        /// Notifies the parent that sizing is started.
+        /// </summary>
+        /// <param name="startPosition"></param>
         public void StartSizing(Point startPosition)
         {
             Parent?.StartSizing(startPosition);
+        }
+
+        /// <summary>
+        /// Refreshes the statues of commands
+        /// </summary>
+        public void RefreshCommandStatus()
+        {
+            // ReSharper disable ExplicitCallerInfoArgument
+            RaisePropertyChanged("CanMoveDown");
+            RaisePropertyChanged("CanMoveUp");
+            // ReSharper restore ExplicitCallerInfoArgument
         }
     }
 }
--- VsIntegration/Spect.Net.VsPackage/ToolWindows/Watch/WatchToolWindowViewModel.cs
+++ VsIntegration/Spect.Net.VsPackage/ToolWindows/Watch/WatchToolWindowViewModel.cs
@@ -8,6 +8,8 @@ namespace Spect.Net.VsPackage.ToolWindows.Watch
     /// </summary>
     public class WatchToolWindowViewModel : SpectrumGenericToolWindowViewModel
     {
+        private const int MIN_LABEL_WIDTH = 40;
+
         private int _labelWidth;
         private bool _sizing;
         private double _startPosition;
@@ -33,8 +35,8 @@ namespace Spect.Net.VsPackage.ToolWindows.Watch
         /// </summary>
         public WatchToolWindowViewModel()
         {
-            //if (IsInDesignMode)
-            //{
+            if (IsInDesignMode)
+            {
                 WatchItems.Add(new WatchItemViewModel());
                 WatchItems.Add(new WatchItemViewModel());
                 WatchItems.Add(new WatchItemViewModel());
@@ -42,11 +44,55 @@ namespace Spect.Net.VsPackage.ToolWindows.Watch
                 {
                     item.Parent = this;
                 }
-            //}
+            }
 
             LabelWidth = 100;
         }
 
+        /// <summary>
+        /// Removes the specified item from the collection
+        /// </summary>
+        /// <param name="item">Item to remove</param>
+        public void RemoveWatchItem(WatchItemViewModel item)
+        {
+            WatchItems.Remove(item);
+            RefreshWatchItems();
+        }
+
+        /// <summary>
+        /// Moves the specified item up
+        /// </summary>
+        /// <param name="item">Item to move up</param>
+        public void MoveUpWatchItem(WatchItemViewModel item)
+        {
+            var itemIndex = WatchItems.IndexOf(item);
+            if (itemIndex < 0 || itemIndex > WatchItems.Count - 2) return;
+            var tmp = WatchItems[itemIndex + 1];
+            WatchItems[itemIndex + 1] = WatchItems[itemIndex];
+            WatchItems[itemIndex] = tmp;
+            RefreshWatchItems();
+        }
+
+        /// <summary>
+        /// Moves the specified item down
+        /// </summary>
+        /// <param name="item">Item to move down</param>
+        public void MoveDownWatchItem(WatchItemViewModel item)
+        {
+            var itemIndex = WatchItems.IndexOf(item);
+            if (itemIndex < 1 || itemIndex > WatchItems.Count - 1) return;
+            var tmp = WatchItems[itemIndex - 1];
+            WatchItems[itemIndex - 1] = WatchItems[itemIndex];
+            WatchItems[itemIndex] = tmp;
+            RefreshWatchItems();
+        }
+
+        /// <summary>
+        /// Processes the command line instruction
+        /// </summary>
+        /// <param name="commandText">Command text to process</param>
+        /// <param name="validationMessage">Validation message in case of error</param>
+        /// <returns>True, if command processed</returns>
         public bool ProcessCommandline(string commandText, out string validationMessage)
         {
             validationMessage = null;
@@ -67,6 +113,7 @@ namespace Spect.Net.VsPackage.ToolWindows.Watch
                         HasError = false
                     };
                     WatchItems.Add(newItem);
+                    RefreshWatchItems();
                     break;
 
                 case WatchCommandType.RemoveWatch:
@@ -74,7 +121,7 @@ namespace Spect.Net.VsPackage.ToolWindows.Watch
                     if (index >= 0 && index < WatchItems.Count)
                     {
                         WatchItems.RemoveAt(index);
-                        RenumberWatchItems();
+                        RefreshWatchItems();
                     }
                     break;
 
@@ -83,10 +130,24 @@ namespace Spect.Net.VsPackage.ToolWindows.Watch
                     break;
 
                 case WatchCommandType.MoveItem:
+                    var index1 = parser.Address;
+                    var index2 = parser.Address2;
+                    if (index1 < 1 || index1 > WatchItems.Count || index2 < 1 || index2 > WatchItems.Count)
+                    {
+                        validationMessage = $"Index values must be between 1 and {WatchItems.Count}.";
+                        return false;
+                    }
+
+                    index1--;
+                    index2--;
+                    var tmp = WatchItems[index1];
+                    WatchItems[index1] = WatchItems[index2];
+                    WatchItems[index2] = tmp;
+                    RefreshWatchItems();
                     break;
 
                 case WatchCommandType.ChangeLabelWidth:
-                    LabelWidth = parser.Address;
+                    LabelWidth = parser.Address < MIN_LABEL_WIDTH ? MIN_LABEL_WIDTH : parser.Address;
                     break;
 
                 default:
@@ -96,6 +157,10 @@ namespace Spect.Net.VsPackage.ToolWindows.Watch
             return true;
         }
 
+        /// <summary>
+        /// Starts sizing the label width
+        /// </summary>
+        /// <param name="startPosition">Mouse position</param>
         public void StartSizing(Point startPosition)
         {
             _sizing = true;
@@ -103,23 +168,73 @@ namespace Spect.Net.VsPackage.ToolWindows.Watch
             _startPosition = startPosition.X;
         }
 
+        /// <summary>
+        /// Handles a sizing when the mouse moves
+        /// </summary>
+        /// <param name="startPosition">Mouse position</param>
         public void HandleSizing(Point startPosition)
         {
             if (!_sizing) return;
             var newSize = _startSize - _startPosition + startPosition.X;
-            LabelWidth = (int)newSize;
+            LabelWidth = newSize < MIN_LABEL_WIDTH ? MIN_LABEL_WIDTH : (int)newSize;
         }
 
+        /// <summary>
+        /// Ends sizing
+        /// </summary>
         public void EndSizing()
         {
             _sizing = false;
         }
 
-        private void RenumberWatchItems()
+        /// <summary>
+        /// Override to handle the start of the virtual machine.
+        /// </summary>
+        /// <remarks>This method is called for the first start, too</remarks>
+        protected override void OnStart()
+        {
+            base.OnStart();
+            foreach (var item in WatchItems)
+            {
+                item.Value = "(not evaluated yet)";
+            }
+        }
+
+        /// <summary>
+        /// Override to handle the paused state of the virtual machine.
+        /// </summary>
+        /// <remarks>This method is called for the first pause, too</remarks>
+        protected override void OnPaused()
+        {
+            base.OnPaused();
+            foreach (var item in WatchItems)
+            {
+                item.Value = "(paused)";
+            }
+        }
+
+        /// <summary>
+        /// Set the machine status when the screen has been refreshed
+        /// </summary>
+        protected override void OnScreenRefreshed()
+        {
+            base.OnScreenRefreshed();
+            if (ScreenRefreshCount % 10 != 0) return;
+            foreach (var item in WatchItems)
+            {
+                item.Value = ScreenRefreshCount.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Renumbers the watch items
+        /// </summary>
+        private void RefreshWatchItems()
         {
             for (var i = 0; i < WatchItems.Count; i++)
             {
                 WatchItems[i].SeqNo = i + 1;
+                WatchItems[i].RefreshCommandStatus();
             }
         }
     }
